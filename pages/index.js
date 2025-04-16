5b59d8554b0731be6f66fc881fe123a379f4b612
EP-SWE <palmcrantz.erik@gmail.com>
2025-04-16 16:23:54 +0200

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>✅ MarkörApp Samverkansövning</h1>
-      <p>Appen är igång. Klart för vidare utveckling!</p>
-    </div>
-  );
-}
